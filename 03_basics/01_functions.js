69e2d7fb488264f78fac316221427aaa12e73ea1
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-12-06 04:00:43 +0530

    learn function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -7,4 +7,24 @@ function loginUserMessage(username) {
 }
 
 
+function calculateCartPrice(...num1) {
+    return num1
+}
+
+const user = {
+    username: "shubham",
+    price: 199
+}
+
+function handleObject(anyobject) {
+    return (`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+const mynewArray = [200, 400, 100, 600]
+
+function returnSecondVAlue(getArray) {
+    return getArray[1]
+}
+
+console.log(returnSecondVAlue(mynewArray))
 
