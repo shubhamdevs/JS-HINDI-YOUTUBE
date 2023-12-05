2ea9b75250139fa34d37d85c73d52de05cafde5d
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-12-06 03:06:40 +0530

    learn object

---
+++ 04_objects.js
@@ -0,0 +1,53 @@
+const tinderUser = new Object()
+
+tinderUser.id = "123abc"
+tinderUser.name = "Shubham"
+
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser)
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+           firstname: "Shubham",
+           lastname: "Biswas"  
+        }
+    }
+}
+
+
+// console.log(regularUser.fullname?.userfullname.firstname)
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = { obj1, obj2}
+
+// const obj3 = Object.assign({}, obj1, obj2)
+
+const obj3 = {...obj1, ...obj2}
+
+// console.log(obj3)
+
+const users = [
+    {
+        id: 1,
+        email: "Shubham@gmail.com"
+    },
+    {
+
+    }
+]
+
+
+users[1].email
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+
+
