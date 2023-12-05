f10a767f45ff218e3a3843bbcb406fdc83a8f097
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-12-06 02:03:34 +0530

    array learning upload

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,35 @@
+// array
+
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+
+console.log(myArr[1])
+
+
+// Array Methods
+
+// myArr.push(6)
+// myArr.push(7)
+
+
+// myArr.unshift(0)
+
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(3))
+
+// const newArr = myArr.join()
+
+// console.log(newArr)
+// console.log(myArr)
+
+
+console.log("A ", myArr);
+
+const myn1
+
+
+
