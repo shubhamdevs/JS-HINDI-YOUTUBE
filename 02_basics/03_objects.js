c4079af9a21df707e91d13f8733808b81a66d486
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-12-06 02:45:54 +0530

    learn about object

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,21 @@
+// singleton
+
+// object literals
+
+// Object.create
+
+
+const mySym = Symbol("key1")
+
+const jsUser = {
+    name: "Shubham",
+
+    age: 24,
+    location: "Kolkata",
+    email: "shubham@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(jsUser["email"])
+
