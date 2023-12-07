1f59dbc73ffda463107936befcbd4cefb7cf9fce
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-12-08 00:53:40 +0530

    learn about scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,41 @@
+// let a = 10
+// const b = 20
+// var c = 30
+
+
+// function one() {
+//     const username = "Shubham"
+
+//     function two() {
+//         const website = "youtube"
+//         console.log(username)
+//     }
+//     // console.log(website)
+
+//     two()
+// }
+
+// one()
+
+if (true) {
+    const username = "shubham"
+    if (username === "shubham") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+function addone(num) {
+    return num + 1
+}
+
+addone(5)
+
+const addTwo = function(num) {
+    return num + 2
+}
+
+addTwo(5)
