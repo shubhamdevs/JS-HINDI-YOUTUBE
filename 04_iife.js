8e349cb1dcb3deaa36eb8d2a78ed12688f1b8700
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-12-08 01:24:21 +0530

    learn about iife

---
+++ 04_iife.js
@@ -0,0 +1,17 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+// name IIFE
+(function chai() {
+    console.log(`DB CONNECTED`)
+})();
+
+
+// simple IIFE
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`)
+})("shubham")
+
+
+
+
+
