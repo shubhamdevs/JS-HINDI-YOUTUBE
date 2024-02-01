bcaddf7e7a92a433f8bd841f4745831b40208d06
SHUBHAM BISWAS <suvm.dev@gmail.com>
2024-02-01 19:05:00 +0530

    learn for loop in js

---
+++ 05_iterations/two.js
@@ -0,0 +1,37 @@
+// While Loop
+
+// while (condition) {
+
+// }
+
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`)
+    index = index + 2
+}
+
+
+let myArray = ["flash", "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value os ${myArray[arr]}`)
+    arr = arr + 1
+}
+
+// do while loop
+
+// do {
+
+// } while (conditon);
+
+// Here we do the task first then check the while condition
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
+// Here we are saying do this untill the condition is meet
